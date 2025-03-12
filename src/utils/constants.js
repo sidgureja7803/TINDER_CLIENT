3322ab2de0f363778d16368022616e73c8ed7ce1
Siddhant Gureja <siddhantgureja39@gmail.com>
2025-03-12 09:40:56 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = "https://dev-tinder-backend-yi46.onrender.com";
-export const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+export const BASE_URL = "https://tinder-server-7rqk.onrender.com";
